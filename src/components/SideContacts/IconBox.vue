5ba2be92592097ef1062e4a6b36b5f70096172a7
Syafiq Aiman <isyafiq@gmail.com>
2024-09-21 18:39:09 +0800

    feat: styles

--- src/components/SideContacts/IconBox.vue
+++ src/components/SideContacts/IconBox.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="h-5 w-5"><slot></slot></div>
+    <div class="h-5 w-5 flex items-center justify-center"><slot></slot></div>
 </template>
 
 <style scoped></style>
