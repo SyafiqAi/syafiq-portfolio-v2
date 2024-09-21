b04e7fed19ad073d1dead5bfc71f9851d4773870
Syafiq Aiman <isyafiq@gmail.com>
2024-09-21 16:57:23 +0800

    feat: home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,45 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+        <main>
+            <div class="flex items-center justify-center h-lvh select-none">
+                <span class="font-extralight">Syafiq</span>
+                <div class="w-10" />
+                <span class="font-extralight text-4xl gradient-animation">
+                    Web Developer
+                </span>
+            </div>
+        </main>
+    </div>
 </template>
+
+<style scoped>
+.gradient-animation {
+    --bg-size: 400%;
+    --color-one: #fa3e00ff;
+    --color-two: #fc216dff;
+    --color-three: #ff04daff;
+    background: linear-gradient(
+            90deg,
+            var(--color-one),
+            var(--color-two),
+            var(--color-three),
+            var(--color-one)
+        )
+        0 0 / var(--bg-size) 100%;
+    color: transparent;
+    background-clip: text;
+}
+
+@media (prefers-reduced-motion: no-preference) {
+    .gradient-animation {
+        animation: move-bg 8s linear infinite;
+    }
+    @keyframes move-bg {
+        to {
+            background-position: var(--bg-size) 0;
+        }
+    }
+}
+</style>
