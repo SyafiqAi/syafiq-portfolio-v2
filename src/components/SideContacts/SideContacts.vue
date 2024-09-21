126b51626ef8377e26ccc66e5636833d8f73c9c3
Syafiq Aiman <isyafiq@gmail.com>
2024-09-21 18:39:28 +0800

    refactor: use v for in icon list

--- src/components/SideContacts/SideContacts.vue
+++ src/components/SideContacts/SideContacts.vue
@@ -1,22 +1,21 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import IconGitHub from "../icons/IconGitHub.vue";
 import IconGmail from "../icons/IconGmail.vue";
 import IconLinkedIn from "../icons/IconLinkedIn.vue";
 import IconBox from "./IconBox.vue";
+
+const icons = ref([IconGitHub, IconGmail, IconLinkedIn]);
 </script>
 
 <template>
     <div class="fixed h-lvh items-center flex">
         <div class="flex flex-col gap-6 ml-4">
-            <IconBox>
-                <IconLinkedIn />
-            </IconBox>
-            <IconBox>
-                <IconGitHub />
-            </IconBox>
-            <IconBox>
-                <IconGmail />
-            </IconBox>
+            <div v-for="(Icon, index) in icons" :key="index">
+                <IconBox>
+                    <component :is="Icon" />
+                </IconBox>
+            </div>
         </div>
     </div>
 </template>
