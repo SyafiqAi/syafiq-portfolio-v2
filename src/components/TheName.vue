7f50ee1e9e8570f281862a4d979b7c6bf6854024
Syafiq Aiman <isyafiq@gmail.com>
2024-09-21 18:45:35 +0800

    refactor: name

---
+++ src/components/TheName.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="flex items-center justify-center h-lvh select-none">
+        <span class="font-extralight">Syafiq</span>
+        <div class="w-10" />
+        <span class="font-extralight text-4xl gradient-animation">
+            Web Developer
+        </span>
+    </div>
+</template>
+<style scoped>
+.gradient-animation {
+    --bg-size: 400%;
+    --color-one: #fa3e00ff;
+    --color-two: #fc216dff;
+    --color-three: #ff04daff;
+    background: linear-gradient(
+            90deg,
+            var(--color-one),
+            var(--color-two),
+            var(--color-three),
+            var(--color-one)
+        )
+        0 0 / var(--bg-size) 100%;
+    color: transparent;
+    background-clip: text;
+}
+
+@media (prefers-reduced-motion: no-preference) {
+    .gradient-animation {
+        animation: move-bg 6s linear infinite;
+    }
+    @keyframes move-bg {
+        to {
+            background-position: var(--bg-size) 0;
+        }
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import SideContacts from "@/components/SideContacts/SideContacts.vue";
+import TheName from "@/components/TheName.vue";
 </script>
 
 <template>
@@ -8,43 +9,7 @@ import SideContacts from "@/components/SideContacts/SideContacts.vue";
             <SideContacts />
         </aside>
         <main>
-            <div class="flex items-center justify-center h-lvh select-none">
-                <span class="font-extralight">Syafiq</span>
-                <div class="w-10" />
-                <span class="font-extralight text-4xl gradient-animation">
-                    Web Developer
-                </span>
-            </div>
+            <TheName />
         </main>
     </div>
 </template>
-
-<style scoped>
-.gradient-animation {
-    --bg-size: 400%;
-    --color-one: #fa3e00ff;
-    --color-two: #fc216dff;
-    --color-three: #ff04daff;
-    background: linear-gradient(
-            90deg,
-            var(--color-one),
-            var(--color-two),
-            var(--color-three),
-            var(--color-one)
-        )
-        0 0 / var(--bg-size) 100%;
-    color: transparent;
-    background-clip: text;
-}
-
-@media (prefers-reduced-motion: no-preference) {
-    .gradient-animation {
-        animation: move-bg 6s linear infinite;
-    }
-    @keyframes move-bg {
-        to {
-            background-position: var(--bg-size) 0;
-        }
-    }
-}
-</style>
