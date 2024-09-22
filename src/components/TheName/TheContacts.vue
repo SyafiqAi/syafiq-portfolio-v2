50509088f6ab9566cd4f6b258b62ea7320998ac9
Syafiq Aiman <isyafiq@gmail.com>
2024-09-22 22:40:46 +0800

    feat: socials links

--- src/components/TheName/TheContacts.vue
+++ src/components/TheName/TheContacts.vue
@@ -3,15 +3,22 @@ import IconBox from "../SideContacts/IconBox.vue";
 import IconGitHub from "../icons/IconGitHub.vue";
 import IconLinkedIn from "../icons/IconLinkedIn.vue";
 import TheGmail from "./TheGmail.vue";
+
+const linkedInLink = "https://www.linkedin.com/in/syafiq-aiman-b10b58303";
+const gitHubLink = "https://github.com/SyafiqAi";
 </script>
 
 <template>
     <div
         class="font-extralight flex gap-10 mt-4 lg:mt-8 opacity-50 group-hover:opacity-100 transition-opacity"
     >
-        <IconBox><IconLinkedIn /></IconBox>
+        <a :href="linkedInLink" target="_blank">
+            <IconBox><IconLinkedIn /></IconBox>
+        </a>
         <TheGmail />
-        <IconBox><IconGitHub /></IconBox>
+        <a :href="gitHubLink" target="_blank">
+            <IconBox><IconGitHub /></IconBox>
+        </a>
     </div>
 </template>
 
