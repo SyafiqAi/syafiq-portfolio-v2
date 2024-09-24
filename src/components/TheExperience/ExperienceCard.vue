3fef583dd24b244d5719382298eb60bfab21252b
Syafiq Aiman <isyafiq@gmail.com>
2024-09-24 21:48:32 +0800

    feat: experience cards

---
+++ src/components/TheExperience/ExperienceCard.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import type { VueElement } from "vue";
+
+export interface Experience {
+    company: string;
+    position: string;
+    date: string;
+    content: VueElement;
+}
+</script>
+
+<template>
+    <div>
+        <div
+            class="p-4 flex flex-col bg-neutral-800 max-w-[500px] mx-2 shadow-black shadow-2xl rounded-xl"
+        >
+            <div class="flex justify-center">
+                <div class="w-fit">
+                    <div class="text-sm">
+                        <slot name="date"></slot>
+                    </div>
+
+                    <div class="text-2xl text-justify">
+                        <slot name="position"></slot>
+                    </div>
+
+                    <div class="text-end">
+                        <slot name="company"></slot>
+                    </div>
+                </div>
+            </div>
+
+            <hr class="my-4 w-[50%] text-center m-auto" />
+
+            <div class="py-6">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
--- src/components/TheExperience/TheExperience.vue
+++ src/components/TheExperience/TheExperience.vue
@@ -1,7 +1,71 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SectionTitle from "../SectionTitle.vue";
+import ExperienceCard from "./ExperienceCard.vue";
+
+interface Experience {
+    position: string;
+    company: string;
+    date: string;
+    content: string[];
+}
+
+const rfLaiyonExperience: Experience = {
+    position: "Front End Web Developer",
+    company: "RF Laiyon Interactive",
+    date: "May 2024 - Present",
+    content: [
+        "Front end development using React - Next.js.",
+        "Worked closely with back-end developers to integrate RESTful APIs.",
+        "Learned authorisation using JWT.",
+        "CI/CD development.",
+        "Contributed to the development of a textbook management system for the public education sector.",
+        "Self-learned React - Next.js, Node.js, Laravel.",
+    ],
+};
+
+const dsmExperience: Experience = {
+    position: "Front End Web Developer",
+    company: "Digital System Malaysia",
+    date: "Oct 2022 - Jan 2023",
+    content: [
+        "Front end development using Vue.js. ",
+        "Worked in a team of 7 developers in an agile development method using Microsoft Azure (DevOps).",
+        "Contributed to the development of an ERP suite for trucking companies.",
+        "Exposure to CI/CD development.",
+        "Self-learned Vue.js, Nuxt.js.",
+    ],
+};
+
+const experiences = [rfLaiyonExperience, dsmExperience];
+</script>
 
 <template>
-    <div class="h-[100svh] box-red">hello</div>
+    <div class="my-20">
+        <SectionTitle
+            text="Experience"
+            class="flex justify-center mt-10 w-full font-extralight"
+        />
+        <div class="flex flex-col gap-y-20 items-center my-16">
+            <ExperienceCard
+                v-for="experience in experiences"
+                :key="experience.company"
+            >
+                <template #position>
+                    {{ experience.position }}
+                </template>
+                <template #company> {{ experience.company }}</template>
+                <template #date> {{ experience.date }}</template>
+                <ul class="list-disc list-outside pl-4 space-y-2">
+                    <li
+                        v-for="(item, index) in experience.content"
+                        :key="index"
+                    >
+                        {{ item }}
+                    </li>
+                </ul>
+            </ExperienceCard>
+        </div>
+    </div>
 </template>
 
 <style scoped></style>
