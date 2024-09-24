e86b31df0d5603cdb0fba23919db07c57854d16f
Syafiq Aiman <isyafiq@gmail.com>
2024-09-24 16:19:43 +0800

    refactor: add gradient text component

---
+++ src/components/GradientText.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+defineProps<{ text: string }>();
+</script>
+
+<template>
+    <p class="gradient-animation">{{ text }}</p>
+</template>
+
+<style scoped>
+.gradient-animation {
+    --bg-size: 400%;
+    --color-one: #fa3e00ff;
+    --color-two: #fc216dff;
+    --color-three: #ff04daff;
+    background: linear-gradient(
+            90deg,
+            var(--color-one),
+            var(--color-two),
+            var(--color-three),
+            var(--color-one)
+        )
+        0 0 / var(--bg-size) 100%;
+    color: transparent;
+    background-clip: text;
+}
+
+@media (prefers-reduced-motion: no-preference) {
+    .gradient-animation {
+        animation: move-bg 6s linear infinite;
+    }
+    @keyframes move-bg {
+        to {
+            background-position: var(--bg-size) 0;
+        }
+    }
+}
+</style>
--- src/components/TheName/TheName.vue
+++ src/components/TheName/TheName.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import GradientText from "../GradientText.vue";
 import TheContacts from "./TheContacts.vue";
 </script>
 
@@ -7,40 +8,12 @@ import TheContacts from "./TheContacts.vue";
         <div class="flex flex-col items-center group py-32">
             <span class="font-extralight mb-2">Syafiq</span>
             <h1
-                class="font-black text-4xl md:text-6xl lg:text-9xl text-nowrap max-w-[100lvw] gradient-animation"
+                class="font-black text-4xl md:text-6xl lg:text-9xl text-nowrap max-w-[100lvw]"
             >
-                Web Developer
+                <GradientText text="Web Developer" />
             </h1>
             <TheContacts />
         </div>
     </div>
 </template>
-<style scoped>
-.gradient-animation {
-    --bg-size: 400%;
-    --color-one: #fa3e00ff;
-    --color-two: #fc216dff;
-    --color-three: #ff04daff;
-    background: linear-gradient(
-            90deg,
-            var(--color-one),
-            var(--color-two),
-            var(--color-three),
-            var(--color-one)
-        )
-        0 0 / var(--bg-size) 100%;
-    color: transparent;
-    background-clip: text;
-}
-
-@media (prefers-reduced-motion: no-preference) {
-    .gradient-animation {
-        animation: move-bg 6s linear infinite;
-    }
-    @keyframes move-bg {
-        to {
-            background-position: var(--bg-size) 0;
-        }
-    }
-}
-</style>
+<style scoped></style>
