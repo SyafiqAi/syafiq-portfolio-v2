d9af3778e210a89d315e15f215be4b1dbed59128
Syafiq Aiman <isyafiq@gmail.com>
2024-09-24 20:13:32 +0800

    refactor: lint

--- src/components/icons/CopyIcon2.vue
+++ src/components/icons/CopyIcon2.vue
@@ -3,6 +3,7 @@
 <template>
     <svg viewBox="0 0 600 600" class="flex-shrink-0" style="flex-shrink: 0">
         <path
+            id="rect347-7"
             fill="currentColor"
             style="
                 stroke-width: 1;
@@ -11,9 +12,9 @@
                 paint-order: stroke fill markers;
             "
             d="M 39.2,110 A 39.20392,39.20392 0 0 0 0,149.2 V 560.8 A 39.20392,39.20392 0 0 0 39.2,600 h 325.13031 a 39.20392,39.20392 0 0 0 39.2,-39.2 V 149.2 a 39.20392,39.20392 0 0 0 -39.2,-39.2 z m 39.2,78.4 H 325.13031 V 521.6 H 78.4 Z"
-            id="rect347-7"
         />
         <path
+            id="rect347-6"
             fill="currentColor"
             style="
                 stroke-width: 1;
@@ -22,7 +23,6 @@
                 paint-order: stroke fill markers;
             "
             d="m 235.66969,0 a 39.20392,39.20392 0 0 0 -39.2,39.2 V 98 h 78.4 V 78.4 H 521.6 V 411.6 H 415.53031 V 490 H 560.8 A 39.20392,39.20392 0 0 0 600,450.8 V 39.2 A 39.20392,39.20392 0 0 0 560.8,0 Z"
-            id="rect347-6"
         />
     </svg>
 </template>
