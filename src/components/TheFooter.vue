c79297a2a2c317d5f61b3d3b2713c8f3cafffafb
Syafiq Aiman <isyafiq@gmail.com>
2024-09-24 15:05:18 +0800

    wip footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,14 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CopyIcon from "./icons/CopyIcon.vue";
+import CopyIcon2 from "./icons/CopyIcon2.vue";
+import IconGitHub from "./icons/IconGitHub.vue";
+import IconLinkedIn from "./icons/IconLinkedIn.vue";
+import IconBox from "./SideContacts/IconBox.vue";
+</script>
 
 <template>
-    <div class="h-dvh gradient-animation flex justify-center">
-        <p class="text-center text-neutral-900 font-black text-6xl md:text-9xl">
-            Let's go. <br />
-            <br />
-            LinkedIn <br />
-            Gmail <br />
-            GitHub <br />
-        </p>
+    <div
+        class="h-dvh bg-gray-500 flex flex-col justify-center text-center text-neutral-900 font-black text-[7dvw]"
+    >
+        <div>Let's go.</div>
+        <div class="flex justify-center">
+            <div class="flex items-center">
+                <IconLinkedIn class="min-h-[70%] min-w-full" />
+            </div>
+            LinkedIn
+        </div>
+        <div class="flex gap-4 md:gap-10 justify-center">
+            email@gmail.com
+            <div class="flex items-center">
+                <CopyIcon2 class="min-h-[70%] min-w-full" />
+            </div>
+        </div>
+        <div class="flex justify-center">
+            <div class="flex items-center">
+                <IconGitHub class="min-h-[70%] min-w-full" />
+            </div>
+            GitHub
+        </div>
     </div>
 </template>
 
---
+++ src/components/icons/CopyIcon2.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts"></script>
+
+<template>
+    <svg viewBox="0 0 600 600" class="flex-shrink-0" style="flex-shrink: 0">
+        <path
+            fill="currentColor"
+            style="
+                color: #000000;
+                stroke-width: 1;
+                stroke-linecap: round;
+                stroke-linejoin: round;
+                paint-order: stroke fill markers;
+            "
+            d="M 39.2,110 A 39.20392,39.20392 0 0 0 0,149.2 V 560.8 A 39.20392,39.20392 0 0 0 39.2,600 h 325.13031 a 39.20392,39.20392 0 0 0 39.2,-39.2 V 149.2 a 39.20392,39.20392 0 0 0 -39.2,-39.2 z m 39.2,78.4 H 325.13031 V 521.6 H 78.4 Z"
+            id="rect347-7"
+        />
+        <path
+            fill="currentColor"
+            style="
+                color: #000000;
+                stroke-width: 1;
+                stroke-linecap: round;
+                stroke-linejoin: round;
+                paint-order: stroke fill markers;
+            "
+            d="m 235.66969,0 a 39.20392,39.20392 0 0 0 -39.2,39.2 V 98 h 78.4 V 78.4 H 521.6 V 411.6 H 415.53031 V 490 H 560.8 A 39.20392,39.20392 0 0 0 600,450.8 V 39.2 A 39.20392,39.20392 0 0 0 560.8,0 Z"
+            id="rect347-6"
+        />
+    </svg>
+</template>
+
+<style scoped></style>
