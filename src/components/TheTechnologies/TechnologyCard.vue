c2d9502c864cff5e32852baeb9899d9f26e652d3
Syafiq Aiman <isyafiq@gmail.com>
2024-11-02 23:17:29 +0800

    feat: remove tiltcard

--- src/components/TheTechnologies/TechnologyCard.vue
+++ src/components/TheTechnologies/TechnologyCard.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { onMounted, useTemplateRef } from "vue";
-import TiltCard from "./TiltCard.vue";
 
 const { technology, src, shadowColor } = defineProps<{
     technology: string;
@@ -21,12 +20,12 @@ onMounted(() => {
             ref="shadowRef"
             class="shadow-lg group-hover:shadow-xl group-hover:animate-pulse rounded-xl size-full absolute shadow-black transition-shadow duration-500 group-hover:shadow-[var(--shadow-color)]"
         />
-        <TiltCard class="h-24 w-24 p-4 text-center align-text-bottom transition-colors duration-300 rounded-xl bg-neutral-800 flex flex-col justify-between select-none">
+        <div class="h-24 w-24 p-4 text-center align-text-bottom transition-all duration-300 rounded-xl bg-neutral-800 flex flex-col justify-between select-none group-hover:scale-110 ">
             <img :src="src" class="h-8" />
             <p>
                 {{ technology }}
             </p>
-        </TiltCard>
+        </div>
     </div>
 </template>
 
