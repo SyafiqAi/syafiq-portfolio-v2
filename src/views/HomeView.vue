1fbff1c22a9a39767dc8f19b1d4e158be57b428e
Syafiq Aiman <isyafiq@gmail.com>
2024-11-03 21:50:50 +0800

    feat: main template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,10 @@
 </script>
 
 <template>
-        <main class="p-[100px]">
-            <h1 class="text-2xl font-bold mb-[20px]">Syafiq's Portfolio</h1>
-            <p>I do web stuff.</p>
-        </main>
+        <div >
+            <main class="mx-auto sm py-[100px] max-w-[640px]">
+                <h1 class="text-2xl font-bold mb-[20px]">Syafiq's Portfolio</h1>
+                <p>I do web stuff.</p>
+            </main>
+        </div>
 </template>
