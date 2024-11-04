e804beb6fc3a89e070fb397c6ffd91a018640022
Syafiq Aiman <isyafiq@gmail.com>
2024-11-04 14:04:24 +0800

    timeline colors

--- src/components/Sections/ExperienceSection.vue
+++ src/components/Sections/ExperienceSection.vue
@@ -6,27 +6,34 @@ import { experiences } from "@/data/experience";
 <template>
     <div>
         <h2>Experience</h2>
-        <TheSection
-            v-for="(experience, index) in experiences"
-            :key="index"
-            :class="(index !== 0 && 'pt-8') + ' relative'"
+
+        <div class="relative">
+            <div
+                class="border-l-[1px] border-gray-400 absolute left-0 top-2 h-full"
+            ></div>
+            <TheSection
+                v-for="(experience, index) in experiences"
+                :key="index"
+                :class="(index !== 0 && '') + ' pb-8 relative'"
             >
-            <div class="border-l-2 border-black w-[full] absolute -left-[17px] -bottom-1 h-full"> </div>
-            <div class="text-xs relative">
-                <div class="size-2 absolute bg-black top-1 -left-5 rounded-full"></div>
-                {{ experience.startDate }} -
-                {{ experience.endDate ?? "Present" }}
-            </div>
-            <h3>{{ experience.company }}</h3>
-            <ul class="ml-6">
-                <li
-                    v-for="(item, index) in experience.content"
-                    :key="index + item"
-                >
-                    {{ item }}
-                </li>
-            </ul>
-        </TheSection>
+                <div class="text-xs relative">
+                    <div
+                        class="size-[7px] absolute bg-gray-600 top-1 -left-[23px] rounded-full"
+                    ></div>
+                    {{ experience.startDate }} -
+                    {{ experience.endDate ?? "Present" }}
+                </div>
+                <h3>{{ experience.company }}</h3>
+                <ul class="ml-6">
+                    <li
+                        v-for="(item, index) in experience.content"
+                        :key="index + item"
+                    >
+                        {{ item }}
+                    </li>
+                </ul>
+            </TheSection>
+        </div>
     </div>
 </template>
 
