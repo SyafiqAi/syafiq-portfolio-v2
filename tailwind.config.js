71224e4d90b2eb514c6a3a4daa34c8b1807a475f
Syafiq Aiman <isyafiq@gmail.com>
2024-11-10 18:38:52 +0800

    refactor: bg gradient colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ export default {
                     300: "rgb(47,117,88)",
                     500: "rgb(0,220,130)",
                 },
+                darkGreen: "rgb(0,15,25)",
+                darkerGreen: "rgb(0,5,10)",
                 darkBlue: "rgb(2,4,32)",
                 offWhite: {
                     1: "rgb(226,232,240)",
