67b528b0a505a23e5b0c244cce996538810c858a
Syafiq Aiman <isyafiq@gmail.com>
2024-11-10 16:56:06 +0800

    feat: title section styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,16 +4,13 @@ import EducationSection from "@/components/Sections/EducationSection.vue";
 import ExperienceSection from "@/components/Sections/Experience/ExperienceSection.vue";
 import SkillsSection from "@/components/Sections/SkillsSection.vue";
 import TheFooter from "@/components/Sections/TheFooter.vue";
+import TitleSection from "@/components/Sections/TitleSection.vue";
 </script>
 
 <template>
     <div class="mx-auto sm px-10 pt-8 max-w-[640px]">
         <main>
-            <h1 class="border-l-8 border-l-neonGreen-500 pl-6">
-                Syafiq's <br />
-                Portfolio
-            </h1>
-            <p class="mb-[30px]">I do web stuff.</p>
+            <TitleSection />
             <ExperienceSection />
             <SkillsSection />
             <EducationSection />
